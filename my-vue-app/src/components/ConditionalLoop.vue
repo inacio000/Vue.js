edc8b3a32af23f39c835fe749e746928663477a0
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-07-29 13:19:56 +0300

    bind concept

--- my-vue-app/src/components/ConditionalLoop.vue
+++ my-vue-app/src/components/ConditionalLoop.vue
@@ -1,9 +1,23 @@
 <template>
+    <h1 :class="{ 'title': true, 'title-home': isHome}">
+        Curso Vue 3
+    </h1>
+    <p :class="['text']">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum optio, commodi id illo nesciunt nostrum a facere consequatur quod debitis iure, consequuntur voluptates aliquid illum nisi officiis provident ad nam?
+    </p>
+    <p :style="styleP">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora molestias, natus sit inventore molestiae necessitatibus debitis itaque corrupti eos eligendi quod adipisci magnam sequi architecto libero minus ad? Maxime, suscipit.
+    </p>
     <div>
         <div 
             v-for="(obj, index) in todos"
-            v-bind:key="obj.id"
+            :key="obj.id"
             class="todos-items" 
+        >
+        <img 
+            v-if="obj.imgSrc"
+            :src="obj.imgSrc" 
+            alt=""
         >
             {{ index }} - {{ obj.title }}
         </div>
@@ -15,18 +29,24 @@
         name: 'ConditionalLoop',
         data() {
             return {
+                styleP: {color: 'white', backgroundColor: 'black', 'padding': '5px 0', fontSize: '20px'},
+                isHome: false,
+                classVar: 'title',
+                pClass: 'text',
                 todos: [
                     {
                         "userId": 1,
                         "id": 1,
                         "title": "delectus aut autem",
-                        "completed": false
+                        "completed": false,
+                        "imgSrc": 'https://placeholder.com/100',
                     },
                     {
                         "userId": 1,
                         "id": 2,
                         "title": "quis ut nam facilis et officia qui",
-                        "completed": false
+                        "completed": false,
+                        "imgSrc": 'https://placeholder.com/100',
                     },
                     {
                         "userId": 1,
@@ -53,8 +73,23 @@
 </script>
 
 <style>
+    .title-home {
+       font-size: 50px; 
+       color: black;
+    }
+
     .todos-items {
         background-color: cadetblue;
         margin-bottom: 5px;
+    }
+
+    .title {
+        font-size: 40px;
+        color: aliceblue;
+    }
+
+    .text {
+        color: azure;
+        font-size: 20px;
     }
 </style>
