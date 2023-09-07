1a94324f84b7307c74d041dbb5fc698dde611ab7
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-09-07 21:33:26 +0300

    computed concepts

--- app.js
+++ app.js
@@ -8,6 +8,30 @@ const app = Vue.createApp({
             age: 25,
             showTitle: true,
             showSubMenu: false,
+            hamburgerIcon: '/icons/hamburger.png',
+            closeIcon: '/icons/close.png',
+            x: 0,
+            y: 0,
+            books: [
+                {
+                    title: 'name of the wind',
+                    author: 'patrick rothfuss',
+                    image: 'https://raw.githubusercontent.com/iamshaunjp/Vue-3-Firebase/lesson-13/assets/1.jpg',
+                    isFav: true
+                },
+                {
+                    title: 'the way of kings',
+                    author: 'brandon sanderson',
+                    image: 'https://raw.githubusercontent.com/iamshaunjp/Vue-3-Firebase/lesson-13/assets/2.jpg',
+                    isFav: false
+                },
+                {
+                    title: 'the final empire',
+                    author: 'brandon sanderson',
+                    image: 'https://raw.githubusercontent.com/iamshaunjp/Vue-3-Firebase/lesson-13/assets/3.jpg',
+                    isFav: false
+                },
+            ]
         }
     },
     methods: {
@@ -25,9 +49,27 @@ const app = Vue.createApp({
         },
         toggleButton() {
             this.showSubMenu = !this.showSubMenu
-            // console.log('toggleButton')
+        },
+        handleEvents(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                alert(data)
+            }
+        },
+        handleMouseMove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+        },
+        toggleCard(book) {
+            book.isFav = !book.isFav
+            // console.log('clicked')
         }
     },
+    computed: {
+        filteredBook() {
+            return this.books.filter((book) => book.isFav)
+        }
+    }
 })
 
 app.mount('#app')
