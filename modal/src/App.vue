92d6220bf1d4a92a64eb05b2910abf27af5b2023
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-09-11 01:31:08 +0300

    emitting custom events and click events modifiers

--- modal/src/App.vue
+++ modal/src/App.vue
@@ -3,11 +3,18 @@
     <h1 class="w-96 m-auto text-3xl font-bold text-slate-700 border-b-2">{{ title }}</h1>
     <!-- <input type="text" ref="name" class="w-96 mx-auto">
     <button @click="handleClick" class="bg-purple-400 w-96 mx-auto">Click</button> -->
-    <MyModal 
-      :header="header"
-      :text="text"
-      theme="sale"
-    />
+    <div v-if="showModal">
+      <MyModal
+        :header="header"
+        :text="text"
+        theme="sale"
+        @close="toggleModal"
+      />
+    </div>
+    <button 
+      @click="toggleModal"
+      class="w-28 p-2 rounded-lg mx-auto bg-blue-500"
+    >Show Modal</button>
   </div>
 </template>
 
@@ -21,9 +28,15 @@ export default {
     return {
       title: 'Vue App',
       header: 'Sign up for the Giveaway',
-      text: 'Grad your ninja swag for half price!'
+      text: 'Grad your ninja swag for half price!',
+      showModal: false
     }
   },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    }
+  }
   // methods: {
   //   handleClick() {
   //     console.log(this.$refs.name)
--- modal/src/components/MyModal.vue
+++ modal/src/components/MyModal.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="w-full h-full top-0 fixed bg-modalbg">
-        <div :class="{'w-96 h-40 p-5 my-24 mx-auto flex flex-col justify-center gap-8 bg-white rounded-lg': true, sale: theme === 'sale'}">
+    <div @click.self="closeModal" class="w-full h-full top-0 fixed bg-modalbg">
+        <div :class="{'w-96 h-40 p-5 my-24 mx-auto flex flex-col justify-center gap-8 bg-white rounded-lg relative': true, sale: theme === 'sale'}">
             <h1 class="text-3xl text-titlecolor font-bold">{{ header }}</h1>
             <p>{{ text }}</p>
+            <span @click="closeModal" class="cursor-pointer absolute top-0 right-0 p-2">X</span>
         </div>
     </div>
 </template>
@@ -13,7 +14,12 @@
             'header',
             'text',
             'theme',
-        ]
+        ],
+        methods: {
+            closeModal() {
+                this.$emit('close')
+            }
+        }
     }
 </script>
 
