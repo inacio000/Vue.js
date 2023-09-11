7f163d5950497f5e1d14c20d16934ba7d0457094
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-09-11 13:59:24 +0300

    slot concepts

--- modal/src/components/MyModal.vue
+++ modal/src/components/MyModal.vue
@@ -1,35 +1,37 @@
 <template>
     <div @click.self="closeModal" class="w-full h-full top-0 fixed bg-modalbg">
-        <div :class="{'w-96 h-40 p-5 my-24 mx-auto flex flex-col justify-center gap-8 bg-white rounded-lg relative': true, sale: theme === 'sale'}">
-            <h1 class="text-3xl text-titlecolor font-bold">{{ header }}</h1>
-            <p>{{ text }}</p>
-            <span @click="closeModal" class="cursor-pointer absolute top-0 right-0 p-2">X</span>
+        <div
+            :class="{ 'w-96 h-fit p-5 my-24 mx-auto flex flex-col justify-center gap-2 bg-white rounded-lg relative': true, sale: theme === 'sale' }">
+            <slot></slot>
+            <div>
+                <slot name="links"></slot>
+            </div>
+            <span @click="closeModal"
+                class="cursor-pointer absolute top-0 right-0 m-2 px-1 border rounded hover:bg-gray-100 duration-300">x</span>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        props: [
-            'header',
-            'text',
-            'theme',
-        ],
-        methods: {
-            closeModal() {
-                this.$emit('close')
-            }
+export default {
+    props: [
+        'theme',
+    ],
+    methods: {
+        closeModal() {
+            this.$emit('close')
         }
     }
+}
 </script>
 
 <style scoped>
-    .sale {
-        background: crimson;
-        color: white;
-    }
+.sale {
+    background: crimson;
+    color: white;
+}
 
-    .sale h1 {
-        color: white;
-    }
+.sale h1 {
+    color: white;
+}
 </style>
