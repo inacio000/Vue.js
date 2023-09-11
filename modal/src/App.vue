7ff6fc48dab25bcc7900f5311c7569e20c182716
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-09-11 14:00:05 +0300

    add second Modal (challenge)

--- modal/src/App.vue
+++ modal/src/App.vue
@@ -5,36 +5,68 @@
     <button @click="handleClick" class="bg-purple-400 w-96 mx-auto">Click</button> -->
     <div v-if="showModal">
       <MyModal
-        :header="header"
-        :text="text"
-        theme="sale"
+        theme=""
         @close="toggleModal"
-      />
+      >
+        <h1 class="text-3xl text-titlecolor font-bold">Welcome To Our Site</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit illo quae maiores possimus.</p>
+        <template v-slot:links>
+          <div class="flex justify-around">
+            <a class="" href="/"><button class="border rounded-lg p-2 hover:bg-gray-100 duration-500">Sign Up Now</button></a>
+            <a class="" href="/"><button class="border rounded-lg p-2 hover:bg-gray-100 duration-500">More Info.</button></a>
+          </div>
+        </template>
+      </MyModal>
     </div>
+    
+    
+    <div v-if="showSecondModal">
+      <MySecondModal @close2="toggleSecondModal">
+        <h2 class="text-3xl text-titlecolor font-bold">Welcome To Second Modal</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ex asperiores numquam quas aspernatur.</p>
+        <template v-slot:links>
+          <div class="flex justify-around">
+            <a class="" href="/"><button class="border rounded-lg p-2 hover:bg-gray-100 duration-500">Sign Up Now</button></a>
+            <a class="" href="/"><button class="border rounded-lg p-2 hover:bg-gray-100 duration-500">More Info.</button></a>
+          </div>
+        </template>
+      </MySecondModal>
+    </div>
+    
     <button 
       @click="toggleModal"
       class="w-28 p-2 rounded-lg mx-auto bg-blue-500"
     >Show Modal</button>
+    
+    <button 
+    @click="toggleSecondModal"
+    class="p-2 rounded-lg mx-auto bg-blue-500"
+  >Show Second Modal</button>
   </div>
 </template>
 
 <script>
 import MyModal from "@/components/MyModal.vue"
+import MySecondModal from './components/MySecondModal.vue'
 
 export default {
   name: 'App',
-  components: { MyModal },
+  components: { MyModal, MySecondModal },
   data() {
     return {
       title: 'Vue App',
       header: 'Sign up for the Giveaway',
       text: 'Grad your ninja swag for half price!',
-      showModal: false
+      showModal: false,
+      showSecondModal: false,
     }
   },
   methods: {
     toggleModal() {
       this.showModal = !this.showModal;
+    },
+    toggleSecondModal() {
+      this.showSecondModal = !this.showSecondModal
     }
   }
   // methods: {
---
+++ modal/src/components/MySecondModal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div @click.self="closeSecondModal" class="w-full h-full top-0 fixed bg-modalbg">
+        <div
+            :class="{ 'w-96 h-fit p-5 my-24 mx-auto flex flex-col justify-center gap-2 bg-white rounded-lg relative': true, sale: theme === 'sale' }">
+            <slot></slot>
+            <div>
+                <slot name="links"></slot>
+            </div>
+            <span @click="closeSecondModal"
+                class="cursor-pointer absolute top-0 right-0 m-2 px-1 border rounded hover:bg-gray-100 duration-300">x</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        closeSecondModal() {
+            this.$emit('close2')
+        }
+    }
+}
+</script>
