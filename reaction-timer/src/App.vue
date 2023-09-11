20c894446e9100a0716e58c7edf6cc52d2599fc1
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-09-12 00:40:16 +0300

    Reaction timer project

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1 class="text-3xl font-bold">In Reaction Timer</h1>
+  <button type="button" @click="start" 
+    :disable="isPlaying"
+    class="w-20 bg-white p-2 mt-4 rounded disabled:bg-transparent"
+  >Play</button>
+  <BlockButton v-if="isPlaying" :delay="delay"/>
+</template>
+
+<script>
+import BlockButton from '@/components/BlockButton.vue'
+
+export default {
+  name: 'App',
+  components: { BlockButton },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ reaction-timer/src/components/BlockButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="w-80 bg-blockBg text-white text-center py-20 rounded-lg border my-20 mx-auto" v-if="showBlock">Click me!</div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+        console.log('component mounted')
+        setTimeout(() => {
+            this.showBlock = true;
+            this.startTimer()
+            // console.log(this.delay)
+        }, this.delay)
+    },
+    // updated() {
+    //     console.log('component updated')
+    // },
+    // unmounted() {
+    //     console.log('component unmounted')
+    // },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+        }
+    }
+}
+</script>
