45658aebd7ea05e652b7ef8d4b2c08e61ca05ed3
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-09-12 19:21:24 +0300

    Custom events with data and create result component

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -4,25 +4,35 @@
     :disable="isPlaying"
     class="w-20 bg-white p-2 mt-4 rounded disabled:bg-transparent"
   >Play</button>
-  <BlockButton v-if="isPlaying" :delay="delay"/>
+  <BlockButton v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <ResultButton v-if="showResult" :score="score"/>
 </template>
 
 <script>
 import BlockButton from '@/components/BlockButton.vue'
+import ResultButton from './components/ResultButton.vue'
 
 export default {
   name: 'App',
-  components: { BlockButton },
+  components: { BlockButton, ResultButton },
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResult: false
     }
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
+      this.showResult = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResult = true
     }
   },
 }
---
+++ reaction-timer/src/components/ResultButton.vue
@@ -0,0 +1,9 @@
+<template>
+      <p class="mt-4">Reaction Time: {{ score }} ms</p>
+</template>
+
+<script>
+export default {
+    props: ['score']
+}
+</script>
