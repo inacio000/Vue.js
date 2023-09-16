06aa50acb5cc9398d75e6ea208b6217fa5045c3f
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-09-16 03:42:00 +0300

    Add email input

--- web-form/src/App.vue
+++ web-form/src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SignupForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignupForm from "@/components/SignupForm.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { SignupForm }
 }
 </script>
 
---
+++ web-form/src/components/SignupForm.vue
@@ -0,0 +1,14 @@
+<template>
+    <form class="max-w-md my-6 mx-auto bg-white text-start p-6 rounded-lg">
+        <label class="text-labelColor inline-block mx-0 mt-5 mb-4 text-sm uppercase font-bold">Email:</label>
+        <input 
+          type="email" 
+          required
+          class="w-full block px-2 py-3 box-border border-b-2 border-inputBorder text-inputColor"
+        >
+      </form>
+</template>
+
+<script>
+export default {}
+</script>
