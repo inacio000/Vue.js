585de537863c49b2c8c6a92fc70c23120921efa2
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-09-18 23:11:21 +0300

    two way data binding concepts

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -4,11 +4,35 @@
         <input 
           type="email" 
           required
+          v-model="email"
           class="w-full block px-2 py-3 box-border border-b-2 border-inputBorder text-inputColor"
         >
+        <input 
+        type="password"  
+        required
+        v-model="password"
+        class="w-full block px-2 py-3 box-border border-b-2 border-inputBorder text-inputColor mb-6"
+      >
+      <label>Role: </label>
+      <select v-model="role" class="w-full block px-2 py-3 box-border border-b-2 border-inputBorder text-inputColor">
+        <option value="developer">Web Developer</option>
+        <option value="designer">Web Designer</option>
+      </select>
       </form>
+
+      <p>Email: {{ email }}</p>
+      <p>Email: {{ password }}</p>
+      <p>Role: {{ role }}</p>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      role: ''
+    }
+  }
+}
 </script>
