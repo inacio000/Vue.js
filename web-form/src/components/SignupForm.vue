b913b97170ce6dcc12e79d7f436b3b73e3bad676
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-09-19 13:56:14 +0300

    multiple checkbox input

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -1,15 +1,26 @@
 <template>
   <form class="max-w-md my-6 mx-auto bg-white text-start p-6 rounded-lg">
-    <label class="text-labelColor inline-block mx-0 mt-5 mb-4 text-sm uppercase font-bold">Email:</label>
+    <label class="text-labelColor inline-block mt-2 text-sm uppercase font-bold">Email:</label>
     <input type="email" required v-model="email"
-      class="w-full block px-2 py-3 box-border border-b-2 border-inputBorder text-inputColor">
+      class="w-full block px-2 py-3 box-border border-b-2 border-inputBorder text-inputColor mb-6">
+
+    <label class="text-labelColor inline-block text-sm uppercase font-bold">Password:</label>
     <input type="password" required v-model="password"
       class="w-full block px-2 py-3 box-border border-b-2 border-inputBorder text-inputColor mb-6">
-    <label class="text-labelColor inline-block mx-0 mt-5 mb-4 text-sm uppercase font-bold">Role: </label>
+
+    <label class="text-labelColor inline-block mx-0 mt-5 mb-2 text-sm uppercase font-bold">Role: </label>
+    
     <select v-model="role" class="w-full block px-2 py-3 box-border border-b-2 border-inputBorder mb-6">
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
+
+    <label class="text-labelColor inline-block text-sm uppercase font-bold">Sills:</label>
+    <input type="text" v-model="tempskill" @keyup="addskill"
+      class="w-full block px-2 py-3 box-border border-b-2 border-inputBorder text-inputColor">
+    <div v-for="skill in skills" :key="skill" class="inline-block mt-5 mr-2 py-2 px-4 bg-inputBorder border rounded-3xl bold cursor-pointer text-inputColor uppercase">
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
 
     <div>
       <input type="checkbox" required v-model="terms" class="inline-block w-8 mr-4 relative">
@@ -47,7 +58,26 @@ export default {
       password: '',
       role: '',
       terms: false,
-      names: []
+      names: [],
+      tempskill: '',
+      skills: []
+    }
+  },
+  methods: {
+    addskill(e) {
+      if(e.key === ' ' && this.tempskill) {
+        if(!this.skills.includes(this.tempskill)) {
+          this.skills.push(this.tempskill)
+        }
+        this.tempskill = ''
+      }
+      // console.log(e)
+    },
+
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((item) => {
+        return skill !== item
+      })
     }
   }
 }
