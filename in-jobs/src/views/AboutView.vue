7bc4981752df11323b53de4a512e2ba13594619c
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-10-02 00:57:20 +0300

    refactored project

---
+++ in-jobs/src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="text-2xl font-bold mb-2">About Page</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae numquam esse voluptatem, atque ducimus asperiores unde necessitatibus dolorum placeat earum fugiat amet neque officia provident praesentium veritatis ipsa a ad.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae numquam esse voluptatem, atque ducimus asperiores unde necessitatibus dolorum placeat earum fugiat amet neque officia provident praesentium veritatis ipsa a ad.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'about',
+}
+</script>
